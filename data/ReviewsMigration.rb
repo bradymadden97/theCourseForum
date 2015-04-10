f521f5e13d0c2e223fc3ada019ae8feda8f417d7
Lawrence Hook <lawrencehook@gmail.com>
2015-04-10 02:49:55 -0400

    Added loggingand better error handling! Import has issue with time parsing. Import is also selective about which reviews it works with. For example, of the first five reviews, only 1 and 3 are actually added. #102

--- data/ReviewsMigration.rb
+++ data/ReviewsMigration.rb
@@ -16,138 +16,186 @@
 # FasterCSV library for ease of parsing, handles commas nested inside quotes too
 require 'csv'
 
-# part1 - DUMP reviews
-def dump_reviews
+puts "Dump or Import: (d/i)"
+dori = gets.chomp
+
+if dori == 'd'
+	# DUMP reviews
 
 	# For timing purposes
-	total_time = Time.now
+	start_time = Time.now
+
+	# open log file
+	log = File.open("#{Rails.root.to_s}/data/ReviewsDumpLog_#{start_time.strftime("%Y.%m.%d-%H:%M")}.log", 'w')
+
+	log.puts "DUMPING REVIEWS"
 
 	# Open ReviewsDump.csv file
 	reviews_dump = File.open("#{Rails.root.to_s}/data/ReviewsDump.csv", 'w')
 
-	# Header for columns
-	reviews_dump.puts "id, comment, course_professor_id, student_id, " \
-			"semester_id, created_at, updated_at, professor_rating," \
-			"enjoyability, difficulty, amount_reading, amount_writing, " \
-			"amount_group, amount_homework, only_tests, recommend, ta_name, " \
-			"course_mnemonic, course_number, professor_firstname, professor_lastname"
+	# Wanted information
+	columns = [
+		:id,
+		:comment, 
+		:course_professor_id,
+		:student_id,
+		:semester_id,
+		:created_at,
+		:updated_at,
+		:professor_rating,
+		:enjoyability,
+		:difficulty,
+		:amount_reading,
+		:amount_writing,
+		:amount_group,
+		:amount_homework,
+		:only_tests,
+		:recommend,
+		:ta_name
+	]
+
+	indirects = [
+		:mnemonic,
+		:course_number,
+		:first_name,
+		:last_name
+	]
+
+
+	# Header
+	header = columns + indirects
+	reviews_dump.puts header.join(',')
 
 	# Get all reviews
 	Review.all.each do |review|
-		# professor assoc. w/ review
-		theProf = Professor.find_by_id(review.professor_id)
-		
-		# course assoc. w/ review
-		theCourse = Course.find_by_id(review.course_id)
-		
-		# Watch for nil course
-		if theCourse
-			# subdepartment assoc. w/ course
-			theSubdep = Subdepartment.find_by_id(theCourse.subdepartment_id)
-		end
+		log.puts "Dumping review #{review.id}..."
 
-		# All the info needed that is assessible directly from review
-		reviews_info = [review.id, review.comment, review.course_professor_id, review.student_id,
-				review.semester_id, review.created_at, review.updated_at, review.professor_rating,
-				review.enjoyability, review.difficulty, review.amount_reading, review.amount_writing,
-				review.amount_group, review.amount_homework, review.only_tests, review.recommend, review.ta_name]
+		# Retrieve professor assoc. w/ review
+		# 	While logging errors 
+		if review.professor_id
+			# Catch the case of a faulty professor id
+			begin
+				the_prof = Professor.find(review.professor_id)
+			rescue ActiveRecord::RecordNotFound => er
+				log.puts "\t#{er}"
+				log.puts "\tCaused by professor_id #{review.professor_id}"
+				log.puts "Skipping review #{review.id}"
+				next
+			end
+		else
+			log.puts "\tError: NO PROFESSOR"
+			log.puts "Skipping review #{review.id}"
+			next
+		end
+		
+		# Retrieve course assoc. w/ review
+		# 	while logging errors
+		if review.course_id
+			# Catch the case of faulty course id
+			begin
+				the_course = Course.find(review.course_id)
+			rescue ActiveRecord::RecordNotFound => er
+				log.puts "\t#{er}"
+				log.puts "\tCaused by course_id #{course.professor_id}"
+				log.puts "Skipping review #{review.id}"		
+				next		
+			end
+		else
+			# Review without a course is useless. There is one such review
+			log.puts "\tError: NO COURSE"
+			log.puts "Deleting review #{review.id}"
+			review.delete
+			next
+		end
+		
+		the_subdep = Subdepartment.find(the_course.subdepartment_id)
+
+		# Add the wanted columns of review
+		reviews_info = columns.map do |column|
+			review.send(column)
+		end
 
 		# Add the "indirect" data (i.e. course_mnemonic, course_number, professor_firstname, professor_lastname)
-		# Avoid the errors of calling methods on nil objects -- in the case that a review does not have an associated professor or course
-		if theSubdep
-			reviews_info << theSubdep.mnemonic
-		else
-			reviews_info << nil
-		end
-
-		if theCourse
-			reviews_info << theCourse.course_number
-		else
-			reviews_info << nil
-		end
-
-		if theProf
-			reviews_info << theProf.first_name
-			reviews_info << theProf.last_name
-		else
-			reviews_info << nil
-			reviews_info << nil
-		end
+		reviews_info << the_subdep.mnemonic
+		reviews_info << the_course.course_number
+		reviews_info << the_prof.first_name
+		reviews_info << the_prof.last_name
 
 		# At last, write the info of each review to the ReviewsDump.csv file
 		reviews_dump.puts reviews_info.to_csv(:quote_char => "'")
 
+		log.puts "Finished review #{review.id}."
+
 	end
-# How long did this take?
-puts "Dump Reviews - Total Runtime: #{(Time.now - total_time) / 60} minutes"
 
-end
+	# Close log file
+	log.puts "All done!"
+	log.close
 
-# part2 - IMPORT reviews
-def import_reviews
-
-	# puts "Warning: This method drops all current reviews in the database. Continue? (y/n)"
-	# warning = gets.chomp
-	# if warning == 'n'
-	# 	abort("Aborting...")
-	# else
-	# 	if warning == 'y'
-	# 		puts 'Proceeding...'
-	# 	else
-	# 		puts 'Bad input: aborting...'
-	# 	end
-	# end
-
-	# Review.delete_all
-
+	# How long did this take?
+	puts "Dump Reviews - Total Runtime: #{(Time.now - start_time) / 60} minutes"
+	
+elsif dori == 'i'
+	# IMPORT reviews
+	
 	# For timing purposes
-	total_time = Time.now
+	start_time = Time.now
+
+	# open log file
+	log = File.open("#{Rails.root.to_s}/data/ReviewsDumpLog_#{start_time.strftime("%Y.%m.%d-%H:%M")}.log", 'w')
+
+	log.puts "IMPORTING REVIEWS"
 
 	# Open ReviewsDump.csv file
-	reviews_dump = File.open("#{Rails.root.to_s}/data/ReviewsDump.csv", 'r')
+	file = File.open("#{Rails.root.to_s}/data/ReviewsDump.csv", 'r')
 
-	reviews_dump.each do |line|
-		# Parse line into array, raw
-		raw = CSV.parse_line(line)
+	# Read in each line of the csv and create the review
+	file.each do |line|
+		# Try to catch CSV malformed lines and gracefully log them
+		begin
+			# Parse line into array
+			raw = CSV.parse_line(line)
 
-		# Skip the header line
-		if raw[0] == 'id'
-			next
-		else
-			data = {
-				"id" => raw[0],
-				"comment" => raw[1],
-				"course_professor_id" => raw[2],
-				"student_id" => raw[3],
-				"semester_id" => raw[4],
-				# "created_at" => raw[5],
-				# "updated_at" => raw[6],
-				"professor_rating" => raw[7],
-				"enjoyability" => raw[8],
-				"difficulty" => raw[9],
-				"amount_reading" => raw[10],
-				"amount_writing" => raw[11],
-				"amount_group" => raw[12],
-				"amount_homework" => raw[13],
-				"only_tests" => raw[14],
-				"recommend" => raw[15],
-				"ta_name" => raw[16],
-				"course_mnemonic" => raw[17],
-				"course_number" => raw[18],
-				"professor_firstname" => raw[19],
-				"professor_lastname" => raw[20]
-			}
+			# Skip the header line
+			if raw[0] == 'id'
+				next
+			else
+				data = {
+					:id => raw[0],
+					:comment => raw[1],
+					:course_professor_id => raw[2],
+					:student_id => raw[3],
+					:semester_id => raw[4],
+					:created_at => raw[5],
+					:updated_at => raw[6],
+					:professor_rating => raw[7],
+					:enjoyability => raw[8],
+					:difficulty => raw[9],
+					:amount_reading => raw[10],
+					:amount_writing => raw[11],
+					:amount_group => raw[12],
+					:amount_homework => raw[13],
+					:only_tests => raw[14],
+					:recommend => raw[15],
+					:ta_name => raw[16],
+					:course_mnemonic => raw[17],
+					:course_number => raw[18],
+					:professor_firstname => raw[19],
+					:professor_lastname => raw[20]
+				}
+			end
+
+			log.puts "Importing review #{data[:id]}..."
 
 			# Try to find course
-			course = Course.find_by_mnemonic_number(data["course_mnemonic"], data["course_number"])
+			course = Course.find_by_mnemonic_number(data[:course_mnemonic], data[:course_number])
 
 			# Declare Professor
 			professor = nil
 
 			# Try to find professor
-			Professor.where(:first_name => data["professor_firstname"], :last_name => data["professor_lastname"]).each do |temp_prof|
+			Professor.where(:first_name => data[:professor_firstname], :last_name => data[:professor_lastname]).each do |temp_prof|
 				# Operating under the assumption that there are no professors with the same name that have taught the same course.
-				# Hopefully this will always be true
 				if temp_prof.courses_list.include?(course)
 					professor = temp_prof
 					break
@@ -155,53 +203,48 @@ def import_reviews
 			end
 
 			# Watch for nil objects
-			if course
-				data["course_id"] = course.id
-			end
-			if professor
-				data["professor_id"] = professor.id
-			end
+			data[:course_id] = course.id if course
+			data[:professor_id] = professor.id if professor
 
 			# Create the review
 			Review.create({
-				:id => data["id"],
-				:comment => data["comment"],
-				:course_professor_id => data["course_professor_id"],
-				:student_id => data["student_id"],
-				:semester_id => data["semester_id"],
-				:created_at => data["created_at"],
-				:updated_at => data["updated_at"],
-				:professor_rating => data["professor_rating"],
-				:enjoyability => data["enjoyability"],
-				:difficulty => data["difficulty"],
-				:amount_reading => data["amount_reading"],
-				:amount_writing => data["amount_writing"],
-				:amount_group => data["amount_group"],
-				:amount_homework => data["amount_homework"],
-				:only_tests => data["only_tests"],
-				:recommend => data["recommend"],
-				:ta_name => data["ta_name"],
-				:course_id => data["course_id"],
-				:professor_id => data["professor_id"]
+				:id => data[:id],
+				:comment => data[:comment],
+				:course_professor_id => data[:course_professor_id],
+				:student_id => data[:student_id],
+				:semester_id => data[:semester_id],
+				:created_at => data[:created_at],
+				:updated_at => data[:updated_at],
+				:professor_rating => data[:professor_rating],
+				:enjoyability => data[:enjoyability],
+				:difficulty => data[:difficulty],
+				:amount_reading => data[:amount_reading],
+				:amount_writing => data[:amount_writing],
+				:amount_group => data[:amount_group],
+				:amount_homework => data[:amount_homework],
+				:only_tests => data[:only_tests],
+				:recommend => data[:recommend],
+				:ta_name => data[:ta_name],
+				:course_id => data[:course_id],
+				:professor_id => data[:professor_id]
 			})
+			log.puts "Finished review #{data[:id]}."
 
-			puts '...'
+		# If CSV was malformed, log it
+		rescue CSV::MalformedCSVError => er
+			log.puts er.message
+			log.puts "Caused by: #{line}"
 		end
+
+
 	end
 
-# How long did this take?
-puts "Import Reviews - Total Runtime: #{(Time.now - total_time) / 60} minutes"
+	# Close log file
+	log.close
 
-end
+	# How long did this take?
+	puts "Import Reviews - Total Runtime: #{(Time.now - start_time) / 60} minutes"
 
-
-puts "Dump or Import: (d/i)"
-dori = gets.chomp
-
-if dori == 'd'
-	dump_reviews
-elsif dori == 'i'
-	import_reviews
 else
 	puts 'bad input'
 end
