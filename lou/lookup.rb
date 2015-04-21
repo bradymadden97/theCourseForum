43bce458f147d179cd19b82958437cafbb86f502
Alan Wei <awei@rpdway.com>
2015-04-20 20:33:51 -0400

    finished fall 2014 and spring 2015

--- lou/lookup.rb
+++ lou/lookup.rb
@@ -38,7 +38,8 @@ puts "How many? #{ldap_professors.count} total"
 limit = gets.chomp.to_i
 puts "Starting from? (0 indexed)"
 offset = gets.chomp.to_i
-
+puts "Auto-accept prior choices? yn"
+auto_accept = gets.chomp == 'y'
 
 # Log that we are starting LDAP lookups
 ldap_log.puts "Starting LDAP lookup for professor cases from #{offset} to #{offset + limit}"
@@ -74,8 +75,18 @@ ldap_professors[offset..limit - 1].each_index do |index|
 		previous_choice = previous_choices[full_name.to_sym]
 		# If we previously made a choice (not nil)
 		if previous_choice
-			# Set answer to previous choice
-			answer = previous_choice
+			if auto_accept
+				answer = previous_choice
+			else
+				puts "Accept prior? yn: #{previous_choice}"
+				choice = gets.chomp
+				if choice == 'y'
+					answer = previous_choice
+				else
+					puts "Enter new choice:"
+					answer = gets.chomp
+				end
+			end
 			# Log that we chose based on prior analysis
 			ldap_log.puts "Duplicate with #{full_name} resolved with prior: #{answer}"
 		# Otherwise, if we haven't made a choice yet
@@ -98,8 +109,7 @@ ldap_professors[offset..limit - 1].each_index do |index|
 		else
 			groupings = answer.split(' ')
 			groupings.each do |grouping|
-				grouping.scan(/./).map
-				Professor.consolidate(professors.values_at(*grouping.scan(/./)))
+				Professor.consolidate(professors.values_at(*grouping.scan(/./).map(&:to_i)))
 			end
 		end
 		# Log each choice for resuming later
@@ -121,10 +131,66 @@ ldap_professors[offset..limit - 1].each_index do |index|
 	# Only goes here if we are rate limited (too many lookups) or failed to find any matching result
 	else
 		# Log some data into terminal for us to look at
-		puts "Rate limited"
-		puts full_name
+		puts "Error for #{full_name}"
 		puts response
-		break
+		# For terminal output, manually ask the human to decide
+		puts "Analyzing Duplicate Professors #{full_name}"
+		names = full_name.split(' ')
+		# Find all potential matches for this name in the database
+		professors = Professor.where(:first_name => names[0], :last_name => names[-1])
+		# Show spread of subdepartments taught by all professors by this name in the database
+		# Helps when spread is like EVSC and EVEC (both probably the same professor)
+		# puts "Subdepartments Taught: #{professors.map(&:courses).flatten.map(&:subdepartment).map(&:mnemonic).uniq.join(', ')}"
+		puts "Subdepartments Taught by #{professors.count} professors"
+		professors.each_index do |i|
+			print "#{i}) "
+			professor = professors[i]
+			professor.courses.map(&:subdepartment).uniq.each do |subdepartment|
+				print "#{subdepartment.mnemonic}: #{professor.courses_in_subdepartment(subdepartment).count} "
+			end
+			puts
+		end
+		# Ask if we should consolidate into one professor, (y)es(n)o(Q)uit
+		puts "Should we consolidate? #{full_name} ynQ OR grouping (i.e. 02 3)"
+		# If we previously made a choice about this professor, try to fetch it
+		previous_choice = previous_choices[full_name.to_sym]
+		# If we previously made a choice (not nil)
+		if previous_choice
+			puts "Accept prior? yn: #{answer}"
+			choice = gets.chomp
+			if choice == 'y'
+				answer = previous_choice
+			else
+				puts "Enter new choice:"
+				answer = gets.chomp
+			end
+			# Log that we chose based on prior analysis
+			ldap_log.puts "Duplicate with #{full_name} resolved with prior: #{answer}"
+		# Otherwise, if we haven't made a choice yet
+		else
+			# Get user input
+			answer = gets.chomp
+			# Log that we chose based on now analysis
+			ldap_log.puts "Duplicate with #{full_name} resolved with now: #{answer}"
+		end
+		# Show which we chose
+		puts "Choosing #{answer}"
+		# If user chose to consolidate professors into one
+		if answer == 'y'
+			names = full_name.split(' ')
+			Professor.consolidate(professors)
+		# If we chose to quit our analysis
+		elsif answer == 'Q'
+			break
+		elsif answer == 'n'
+		else
+			groupings = answer.split(' ')
+			groupings.each do |grouping|
+				Professor.consolidate(professors.values_at(*grouping.scan(/./).map(&:to_i)))
+			end
+		end
+		# Log each choice for resuming later
+		ldap_choices.puts "#{full_name};#{answer}"
 	end
 	# Sleep for a tiny second to not overwhelm poor LDAP servers
 	sleep(0.001)
